33fe344d192582499800bd36d7efbbbc01ece49e
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-08-30 16:59:42 +0200

    nsc-lib: re-export nsc-core and nsc-parser in Syntax module

---
+++ src/Language/NStar/Syntax.hs
@@ -0,0 +1,10 @@
+module Language.NStar.Syntax
+( -- * Re-exports
+  module Language.NStar.Syntax.Core
+, module Language.NStar.Syntax.Lexer
+, module Language.NStar.Syntax.Parser
+) where
+
+import Language.NStar.Syntax.Lexer
+import Language.NStar.Syntax.Parser
+import Language.NStar.Syntax.Core
