6c280336ecd8b4dddb6039994a6450de935ec1c0
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-09-02 08:43:19 +0200

    nsc-core: add typechecker core

---
+++ lib/nsc-core/src/Language/NStar/Typechecker/Core.hs
@@ -0,0 +1,14 @@
+{-|
+  Module: Language.NStar.Typechecker.Core
+  Description: NStar's typechecking core language
+  Copyright: (c) Mesabloo, 2020
+  License: BSD3
+  Stability: experimental
+-}
+
+module Language.NStar.Typechecker.Core
+( -- * Re-exports
+  module Language.NStar.Syntax.Core
+) where
+
+import Language.NStar.Syntax.Core (Type(..), Kind(..))
