cb7203f567f63883c9e87e011cfedc7422469403
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-09-06 22:41:51 +0200

    nsc-typechecker: allow fetching free variables from types

---
+++ lib/nsc-typechecker/src/Language/NStar/Typechecker/Free.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+{-|
+  Module: Language.NStar.Typechecker.Free
+  Copyright: (c) Mesabloo, 2020
+  License: BSD3
+  Stability: experimental
+-}
+
+module Language.NStar.Typechecker.Free where
+
+import Data.Set (Set)
+import qualified Data.Set as Set (singleton, (\\))
+import qualified Data.Map as Map (foldr)
+import Data.Text (Text)
+import Data.Foldable (fold)
+import Data.Bifunctor (bimap)
+import Language.NStar.Typechecker.Core
+import Data.Located (Located, unLoc)
+
+-- | The class of types whose free variables can be extracted from.
+class Free t where
+  -- | Free variable extractor.
+  freeVars :: t -> Set (Located Text)
+
+instance Free t => Free [t] where
+  freeVars = fold . fmap freeVars
+
+instance (Free a, Free b) => Free (a, b) where
+  freeVars = uncurry (<>) . bimap freeVars freeVars
+
+instance Free (Located Type) where
+  freeVars = freeVars . unLoc
+
+instance Free Type where
+  freeVars (Cons t1 t2)      = freeVars t1 <> freeVars t2
+  freeVars (Var v1)          = Set.singleton v1
+  freeVars (Ptr t1)          = freeVars t1
+  freeVars (SPtr t1)         = freeVars t1
+  freeVars (ForAll binds ty) = freeVars ty Set.\\ freeVars (fst <$> binds)
+  freeVars (Record fields)   = Map.foldr ((<>) . freeVars) mempty fields
+  freeVars (Signed _)        = mempty
+  freeVars (Unsigned _)      = mempty
+  -- Please refrain yourself from putting the two above cases under the same pattern "_".
+  -- Those two are separated in order to keep GHC's warning about incomplete pattern matchings.
+  --
+  -- Yes, it's because I forget to add new types in there everytime.
+
+
+-- No -XPolyKinds, so no need to make an instance for @Free Kind@.
