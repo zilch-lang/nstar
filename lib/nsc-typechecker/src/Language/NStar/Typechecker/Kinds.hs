19fda0546b682aa40a0b7755ce24e034ebe2a5f4
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-09-16 22:07:45 +0200

    nsc-typechecker: prototype kindchecker

--- lib/nsc-typechecker/src/Language/NStar/Typechecker/Kinds.hs
+++ lib/nsc-typechecker/src/Language/NStar/Typechecker/Kinds.hs
@@ -5,7 +5,7 @@
   Stability: experimental
 -}
 
-module Language.NStar.Typechecker.Kinds where
+module Language.NStar.Typechecker.Kinds (kindcheck) where
 
 {-
 
@@ -20,3 +20,92 @@ module Language.NStar.Typechecker.Kinds where
     - `*a` needs `a: T8|Ta` and returns `T8` (on x64)
 
 -}
+
+import Control.Monad.Except
+import Data.Located
+import Language.NStar.Typechecker.Core
+import Text.Diagnose (Diagnostic, diagnostic, (<++>), Report)
+import Data.Bifunctor (first, second)
+import Data.Text (Text)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Control.Applicative (liftA2)
+import Language.NStar.Typechecker.Errors
+
+type Kindchecker a = Except KindcheckError a
+
+data KindcheckError
+  = NotAStackType (Located Kind)
+  | NotADataType (Located Kind)
+  | NotSized (Located Kind)
+  | UnboundTypeVariable (Located Text)
+
+--------------------------------------------------------------------------------------------
+
+kindcheck :: Located Type -> Either (Diagnostic s String m) ()
+kindcheck = second (const ()) . first toDiagnostic . runExcept . kindcheckType mempty
+ where toDiagnostic = (diagnostic <++>) . fromKindcheckError
+
+fromKindcheckError :: KindcheckError -> Report String
+fromKindcheckError (NotAStackType (k :@ p))       = kindIsNotAStackKind k p
+fromKindcheckError (NotADataType (k :@ p))        = kindIsNotADataKind k p
+fromKindcheckError (NotSized (k :@ p))            = kindIsUnsized k p
+fromKindcheckError (UnboundTypeVariable (v :@ p)) = unboundTypeVariable v p
+
+kindcheckType :: Map (Located Text) (Located Kind) -> Located Type -> Kindchecker (Located Kind)
+kindcheckType _ (Signed _ :@ p)            = pure (T8 :@ p)
+kindcheckType _ (Unsigned _ :@ p)          = pure (T8 :@ p)
+    -- TODO: For now we ignore the size of both types, because there are no sized kinds other than @T8@.
+kindcheckType ctx (ForAll newCtx ty :@ _)  = kindcheckType (Map.fromList (first varName <$> newCtx) <> ctx) ty
+  where varName (Var v :@ _) = v
+        varName (t :@ _)     = error $ "Cannot fetch name of non type-variable type '" <> show t <> "'."
+kindcheckType ctx (Var v :@ _)             = maybe (throwError (UnboundTypeVariable v)) pure (Map.lookup v ctx)
+kindcheckType _ (FVar v :@ _)              = throwError (UnboundTypeVariable v)
+kindcheckType ctx (Ptr t :@ p)             = (T8 :@ p) <$ kindcheckType ctx t
+kindcheckType ctx (SPtr t :@ p)            = (Ts :@ p) <$ (requireStackType =<< kindcheckType ctx t)
+kindcheckType ctx (Cons t1 t2 :@ p)        = do
+  liftA2 (*>) requireDataType requireSized =<< kindcheckType ctx t1
+  requireStackType =<< kindcheckType ctx t2
+  pure (Ts :@ p)
+kindcheckType ctx (Record mappings :@ p)   =
+  (Ta :@ p) <$ traverse ((liftA2 (*>) requireDataType requireSized =<<) . kindcheckType ctx) mappings
+       -- FIXME: Kind checking does not take into account the size of the types, so if they are sized, they all are 8-bytes big at the moment.
+
+--------------------------------------------------------------------------------------------
+
+-- | Checks whether a kind indicates a stack type, or not.
+isStackType :: Located Kind -> Bool
+isStackType (Ts :@ _) = True
+isStackType _         = False
+
+-- | Checks whether a kind indicates a data type, or not.
+--
+--   @'isDataType' = 'not' '.' 'isStackType'@
+isDataType :: Located Kind -> Bool
+isDataType = not . isStackType
+
+-- | Checks whether a kind relates to some sized data or not.
+isSized :: Located Kind -> Bool
+isSized (T8 :@ _) = True
+isSized _         = False
+
+-- | Requires a kind to be indicating of a stack type.
+--
+--   Essentially a wrapper around 'isStackType', but in the 'Kindchecker' monad.
+requireStackType :: Located Kind -> Kindchecker ()
+requireStackType k | isStackType k = pure ()
+                   | otherwise     = throwError (NotAStackType k)
+
+-- | Requires a kind to be indicating of a data type.
+--
+--   Essentially a wrapper around 'isDataType', but in the 'Kindchecker' monad.
+requireDataType :: Located Kind -> Kindchecker ()
+requireDataType k | isDataType k = pure ()
+                  | otherwise    = throwError (NotADataType k)
+
+-- | Requires a kind to be sized.
+--
+--   Essentially a wrapper around 'isSized', but in the 'Kindchecker' monad.
+requireSized :: Located Kind -> Kindchecker ()
+requireSized k | isSized k = pure ()
+               | otherwise = throwError (NotSized k)
