47bf891f5cb26c7608866849ebde71efe645f09f
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-09-25 09:54:56 +0200

    nsc-tests: add skeleton for test engine

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+module Main (main) where
+
+import Test.Hspec
+import System.FilePath.Glob (glob)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = glob "./**/*.nst" >>= hspec . tests
+
+tests :: [FilePath] -> Spec
+tests = parallel . foldl (*>) (pure ()) . fmap check
+
+check :: FilePath -> Spec
+check file = pure ()
