acf4e46c681d2e66d68e3e073679605eb751110f
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-09-25 10:37:59 +0200

    nsc-tests: allow tests to fail at specific phases

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TupleSections #-}
+
 module Main (main) where
 
 import Test.Hspec
@@ -8,6 +11,22 @@ import Language.NStar.Syntax (lexFile, parseFile)
 import Language.NStar.Typechecker (typecheck)
 import Data.List (isInfixOf)
 import Text.Diagnose ((<~<), prettyText)
+import Data.Bifunctor (first)
+
+data Error
+  = Lx
+  | Ps
+  | Tc
+  | Any
+  | No
+ deriving Eq
+
+instance Show Error where
+  show Lx = "lexing"
+  show Ps = "parsing"
+  show Tc = "type-checking"
+  show Any = "any"
+  show No = "no"
 
 -----------------------
 
@@ -23,15 +42,35 @@ check :: FilePath -> Spec
 check file = do
   content <- runIO $ Text.readFile file
 
+  -- We have five cases at the moment:
+  -- - a lexing error (error_lx_*)
+  -- - a parsing error (error_ps_*)
+  -- - a typechecking error (error_tc_*)
+  -- - any error (error_*)
+  -- - no error (*)
+  --
+  -- We have to handle those
+
+  let expectedError =
+        if | "error_lx_" `isInfixOf` file -> Lx
+           | "error_ps_" `isInfixOf` file -> Ps
+           | "error_tc_" `isInfixOf` file -> Tc
+           | "error_" `isInfixOf` file    -> Any
+           | otherwise                    -> No
+
   let result = do
-        tokens <- lexFile file content
-        ast <- parseFile file tokens
-        ast <- typecheck ast
+        tokens <- first (, Lx) $ lexFile file content
+        ast <- first (, Ps) $ parseFile file tokens
+        ast <- first (, Tc) $ typecheck ast
         pure ast
 
   specify file $
     case result of
-      Left _ | "error_" `isInfixOf` file -> pure () -- test fails as expected
-      Right _ | "error_" `isInfixOf` file -> expectationFailure ("Test on file '" <> file <> "' should have failed, but passed!")
-      Left diag -> expectationFailure ("Test on file '" <> file <> "' failed with error:\n" <> show (prettyText (diag <~< (file, lines $ Text.unpack content))))
-      Right _ -> pure () -- test passes as expected
+      Left (_, Any)                    -> pure () -- test fails as expected
+      Left (d, e) | expectedError == e -> pure ()
+                  | otherwise          ->
+                      expectationFailure ("Expected to fail in " <> show expectedError <>
+                                               " phase but failed in " <> show e <> " phase with error:\n" <>
+                                               show (prettyText (d <~< (file, lines $ Text.unpack content))))
+      Right _ | expectedError /= No    -> expectationFailure ("Test should have failed in " <> show expectedError <> " phase, but passed all phases!")
+              | otherwise              -> pure () -- test passes as expected
