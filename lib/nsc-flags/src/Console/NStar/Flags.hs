d8bf914e0996212f10499e02fc2f91af235bfbb8
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-09-29 18:25:40 +0200

    nsc-flags: add description for the -f option

--- lib/nsc-flags/src/Console/NStar/Flags.hs
+++ lib/nsc-flags/src/Console/NStar/Flags.hs
@@ -17,7 +17,7 @@ extractFlags = postProcessFlags =<< cmdArgsRun (cmdArgsMode cli)
 cli :: Flags
 cli = Flags
   { files = def &= args &= typ "FILES..."
-  , flags = def &= typ "NAME=VALUE" &= explicit &= name "f" &= name "flag"
+  , flags = def &= typ "NAME=VALUE" &= explicit &= name "f" &= name "flag" &= help "Sets a compiler flag to a given value"
   } &= versionArg [ignore]
     &= helpArg [explicit, name "help", name "h"]
     &= program "nsc"
