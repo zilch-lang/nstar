80dd1dabb70d7afee9245adcbab9f5521a9384a7
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-09-29 17:48:53 +0200

    nsc-flags: parse some flags

---
+++ lib/nsc-flags/src/Console/NStar/Flags.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Console.NStar.Flags
+( extractFlags
+,  -- * Re-exports
+  CompilerFlags(..)
+) where
+
+import Console.NStar.Flags.Internal (Flags(..), CompilerFlags(..))
+import Console.NStar.Flags.Process
+import System.Console.CmdArgs
+
+extractFlags :: IO CompilerFlags
+extractFlags = postProcessFlags =<< cmdArgsRun (cmdArgsMode cli)
+
+cli :: Flags
+cli = Flags
+  { files = def &= args &= typ "FILES..."
+  , flags = def &= typ "NAME=VALUE" &= explicit &= name "f" &= name "flag"
+  } &= versionArg [ignore]
+    &= helpArg [explicit, name "help", name "h"]
+    &= program "nsc"
