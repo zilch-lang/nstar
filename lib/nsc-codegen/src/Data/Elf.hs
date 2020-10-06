1208498cbe584888b49def7d5996eb492f77be2e
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-06 12:31:25 +0200

    nsc-codegen: remove useless .chs extension

--- lib/nsc-codegen/src/Data/Elf.hs
+++ lib/nsc-codegen/src/Data/Elf.hs
@@ -20,8 +20,6 @@ module Data.Elf
 , Object(..)
 ) where
 
-#include <elf.h>
-
 -- For now, let's just focus on the 64 bits format, no matter what the HOST/TARGET arch is.
 
 import Data.Elf.Types
@@ -31,27 +29,11 @@ import Data.Elf.ProgramHeader
 import Data.ByteString (ByteString)
 
 
-{- $e_ident
-
-   The 'e_ident' field contains the ELF magic number and some other info laid out as:
-
-   - Byte number 0: @0x7f@
-   - Bytes number 1-3: @\'E\', \'L\', \'F\'@
-   - Byte number 4: ELF class (32-bit or 64-bit object)
-   - Byte number 5: Data encoding (little endian or big endian)
-   - Byte number 6: File version (must always be @'ev_current'@)
-   - Byte number 7: OS ABI identification (one of 'elfosabi_none', 'elfosabi_sysv')
-   - Byte number 8: ABI version
-   - Bytes number 9-15: @0@-ed padding bytes
--}
-
--------------------------------------------------------------
-
 -- | An object file layout.
 data Object
   = Object
   { o_header    :: Elf64_Ehdr   -- ^ The ELF header
-  , p_table     :: Elf64_Phdr   -- ^ The program header table
-  , s_table     :: Elf64_Shdr   -- ^ The section header table
+  , p_table     :: [Elf64_Phdr] -- ^ Program headers
+  , s_table     :: [Elf64_Shdr] -- ^ Section headers
   , bytes       :: ![UChar]     -- ^ Data stored in the object file
   }
