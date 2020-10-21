8c0fa572a55deceddd2c8bd0b1c8bef08e478dd1
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-21 16:51:45 +0200

    nsc-codegen: introduce more abstraction on the file header

---
+++ lib/nsc-codegen/src/Data/Elf/FileHeader.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE BangPatterns #-}
+
+{-# OPTIONS -Wno-duplicate-exports #-}
+
+module Data.Elf.FileHeader
+( ElfHeader(..)
+, Class(..)
+, Encoding(..)
+, OSABI(..)
+, ObjFileType(..)
+, Arch(..)
+, Version(..)
+, module Data.Elf.FileHeader.Flags
+) where
+
+import Data.Elf.Types
+import Data.Elf.FileHeader.Flags (EFlags)
+import Data.Word (Word8)
+
+
+
+data ElfHeader
+  = ElfHeader
+      !Class         -- ^ ELF class (32-bit or 64-bit object)
+      !Encoding      -- ^ Data encoding (little endian or big endian)
+      OSABI          -- ^ OS ABI identification
+      !UChar         -- ^ ABI version
+      ObjFileType    -- ^ Object file type
+      Arch           -- ^ Architecture
+      !Version       -- ^ Object file version (@'ev_current'@ or @'ev_none'@)
+      !EFlags        -- ^ Processor-specific flags
+
+-- | ELF class
+data Class
+  = C_None  -- ^ Invalid class
+  | C_32    -- ^ 32-bit object
+  | C_64    -- ^ 64-bit object
+
+-- | Data encoding
+data Encoding
+  = D_None    -- ^ Invalid data encoding
+  | D_2LSB    -- ^ 2's complement, little endian
+  | D_2MSB    -- ^ 2's complement, big endian
+
+-- | OS ABI identification
+data OSABI
+  = OSABI_None      -- ^ Unix System V ABI
+  | OSABI_SysV      -- ^ Same as 'OSABI_None'
+
+-- | Legal values for @'e_type'@.
+data ObjFileType
+  = ET_None        -- ^ No file type
+  | ET_Rel         -- ^ Relocatable file
+  | ET_Exec        -- ^ Executable file
+  | ET_Dyn         -- ^ Shared object file
+  | ET_Core        -- ^ Core file
+
+-- | Legal values for @'e_machine'@.
+data Arch
+  = EM_None             -- ^ No machine
+  | EM_sparc            -- ^ SUN SPARC
+  | EM_x86_64           -- ^ AMD x86-64 architecture
+  | EM_arm              -- ^ ARM
+
+data Version
+  = EV_None        -- ^ Invalid ELF version
+  | EV_Current     -- ^ Current version
--- lib/nsc-codegen/src/Data/Elf/Object.hs
+++ lib/nsc-codegen/src/Data/Elf/Object.hs
@@ -3,14 +3,14 @@
 module Data.Elf.Object where
 
 import Data.Elf.Types
-import Data.Elf.FileHeader (Elf64_Ehdr)
+import Data.Elf.FileHeader (ElfHeader)
 import Data.Elf.SectionHeader (Elf64_Shdr)
 import Data.Elf.ProgramHeader (Elf64_Phdr)
 
 -- | An object file layout.
 data Object64
   = Object64
-  { o_header    :: Elf64_Ehdr   -- ^ The ELF header
+  { o_header    :: ElfHeader    -- ^ The ELF header
   , p_table     :: [Elf64_Phdr] -- ^ Program headers
   , s_table     :: [Elf64_Shdr] -- ^ Section headers
   , bytes       :: ![UChar]     -- ^ Data stored in the object file
