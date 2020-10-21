97de9a2d43dee98d005c373cd2197a218a0b119c
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-21 22:05:35 +0200

    nsc-codegen: introduce symbol table in object file

--- lib/nsc-codegen/src/Data/Elf/Object.hs
+++ lib/nsc-codegen/src/Data/Elf/Object.hs
@@ -6,11 +6,13 @@ import Data.Elf.Types
 import Data.Elf.FileHeader (ElfHeader)
 import Data.Elf.SectionHeader (SectionHeader)
 import Data.Elf.ProgramHeader (ProgramHeader)
+import Data.Elf.SymbolTable (ElfSymbol)
 
 -- | An object file layout.
 data Object64
   = Object64
-  { o_header    :: ElfHeader           -- ^ The ELF header
-  , p_table     :: [ProgramHeader]     -- ^ Program headers
-  , s_table     :: [SectionHeader]     -- ^ Section headers
+  { fileHeader    :: ElfHeader           -- ^ The ELF header
+  , segments      :: [ProgramHeader]     -- ^ Program headers
+  , sections      :: [SectionHeader]     -- ^ Section headers
+  , symbols       :: [ElfSymbol]         -- ^ Symbol table
   }
---
+++ lib/nsc-codegen/src/Data/Elf/SymbolTable.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Data.Elf.SymbolTable
+( ElfSymbol(..)
+, SymbolType(..)
+, SymbolBinding(..)
+, SymbolVisibility(..)
+) where
+
+import Data.Elf.Types
+
+-- | Symbol table entry
+data ElfSymbol
+  = ElfSymbol
+      String           -- ^ Symbol name
+      SymbolType       -- ^ Symbol type
+      SymbolBinding    -- ^ Symbol binding
+      SymbolVisibility -- ^ Symbol visibility
+
+-- | Symbol type
+data SymbolType
+  = ST_NoType       -- ^ Symbol type is unspecified
+  | ST_Object       -- ^ Symbol is a data object
+  | ST_Func         -- ^ Symbol is a code object
+  | ST_Section      -- ^ Symbol associated with a section
+  | ST_File         -- ^ Symbol's name is file name
+  | ST_Common       -- ^ Symbol is a common data object
+  | ST_TLS          -- ^ Symbol is a thread-local data object
+
+-- | Symbol binding type
+data SymbolBinding
+  = SB_Local       -- ^ Local symbol
+  | SB_Global      -- ^ Global symbol
+  | SB_Weak        -- ^ Weak symbol
+
+-- | Symbol visibility specification
+data SymbolVisibility
+  = SV_Default         -- ^ Default symbol visibility rules
+  | SV_Internal        -- ^ Processor specific hidden class
+  | SV_Hidden          -- ^ Symbol unavailable in other modules
+  | SV_Protected       -- ^ Not preemptible, not exported
