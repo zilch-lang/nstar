d3ba9c086b3fb5003c3168947e222cb5e89c81b2
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-21 19:53:03 +0200

    nsc-codegen: introduce more abstraction on the section header

--- lib/nsc-codegen/src/Data/Elf/Object.hs
+++ lib/nsc-codegen/src/Data/Elf/Object.hs
@@ -4,14 +4,14 @@ module Data.Elf.Object where
 
 import Data.Elf.Types
 import Data.Elf.FileHeader (ElfHeader)
-import Data.Elf.SectionHeader (Elf64_Shdr)
+import Data.Elf.SectionHeader (SectionHeader)
 import Data.Elf.ProgramHeader (Elf64_Phdr)
 
 -- | An object file layout.
 data Object64
   = Object64
-  { o_header    :: ElfHeader    -- ^ The ELF header
-  , p_table     :: [Elf64_Phdr] -- ^ Program headers
-  , s_table     :: [Elf64_Shdr] -- ^ Section headers
-  , bytes       :: ![UChar]     -- ^ Data stored in the object file
+  { o_header    :: ElfHeader           -- ^ The ELF header
+  , p_table     :: [Elf64_Phdr]        -- ^ Program headers
+  , s_table     :: [SectionHeader]     -- ^ Section headers
+  , bytes       :: ![UChar]            -- ^ Data stored in the object file
   }
---
+++ lib/nsc-codegen/src/Data/Elf/SectionHeader.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Data.Elf.SectionHeader
+( SectionHeader(..)
+, module Data.Elf.SectionHeader.Flags
+) where
+
+import Data.Elf.Types
+import Data.Word (Word8)
+import Data.Elf.SectionHeader.Flags (SFlags)
+
+-- | Section header
+data SectionHeader
+  -- | Section header table entry unused
+  = SNull
+  -- | Program data
+  | SProgBits
+      [UChar]
+      SFlags
+  -- | Program space with no data (bss)
+  | SNoBits
+      [UChar]
+      SFlags
