818cf6469f78b5cc1b0ed0504b8a54389dfe017a
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-22 21:41:58 +0200

    nsc-codegen: use Builder instead of plain ByteString

--- lib/nsc-codegen/src/Language/NStar/CodeGen/Compiler.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Compiler.hs
@@ -1,7 +1,7 @@
 module Language.NStar.CodeGen.Compiler where
 
 import Control.Monad.Writer (Writer, WriterT)
-import Data.ByteString (ByteString)
+import Data.ByteString.Builder (Builder)
 import Language.NStar.CodeGen.Errors
 
-type Compiler a = WriterT [CodegenError] (Writer ByteString) a
+type Compiler a = WriterT [CodegenError] (Writer Builder) a
--- lib/nsc-codegen/src/Language/NStar/CodeGen/Machine.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine.hs
@@ -1,14 +1,16 @@
 module Language.NStar.CodeGen.Machine where
 
 import Language.NStar.Typechecker.Core (TypedProgram)
-import Data.ByteString (ByteString)
+import Data.ByteString.Lazy (ByteString)
+import Data.ByteString.Builder (toLazyByteString)
 import Control.Monad.Writer (runWriter, execWriterT, tell)
 import Language.NStar.CodeGen.Errors
+import Data.Bifunctor (second)
 
 data SupportedArch
   = X64
 
 
 compile :: SupportedArch -> TypedProgram -> ([CodegenError], ByteString)
-compile arch prog = runWriter $ execWriterT case arch of
+compile arch prog = second toLazyByteString $ runWriter $ execWriterT case arch of
   X64 -> error "TODO: opcode compilation not yet implemented for x64"
