c5c7099b140a164f43db9644811f93c26461a739
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-30 16:45:04 +0100

    nsc-codegen: import intermediate codegen format

---
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/Internal/Intermediate.hs
@@ -0,0 +1,18 @@
+module Language.NStar.CodeGen.Machine.Internal.Intermediate
+( InterOpcode(..)
+) where
+
+import Data.Word (Word8)
+import Data.Text (Text)
+
+-- we need an IR where labels still exist, but everything else is already compiled.
+-- that way, we can handle forward and backward jumps easily.
+
+-- ^ Represents an intermediate instruction, where jumps and labels are explicit.
+data InterOpcode
+  -- | Any byte
+  = Byte Word8
+  -- | A label (jump destination)
+  | Label Text
+  -- | An unconditional jump to a given label
+  | Jump Text
