60709cb0740c4131e09c639653d3cafb492c18ad
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-30 22:42:20 +0100

    nsc-codegen: add ELF builder skeleton

---
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Elf/Builder.hs
@@ -0,0 +1,25 @@
+module Language.NStar.CodeGen.Elf.Builder where
+
+import Data.Elf
+import Language.NStar.CodeGen.Machine
+import Data.Bits ((.|.))
+import Language.NStar.CodeGen.Arch (SupportedArch(..))
+import Language.NStar.Typechecker.Core (TypedProgram)
+
+compileToElf :: SupportedArch -> TypedProgram -> Object64
+compileToElf arch prog =
+  Object64
+    (ElfHeader (supportedArchToClass arch) (supportedArchToEncoding arch) OSABI_None 0x0 ET_Exec (supportedArchToArch arch) EV_Current ef_none)
+    [PLoad (section ".text") pf_r]
+    [SProgBits ".text" (compile arch prog) (shf_alloc .|. shf_execinstr)]
+    [] -- Leave empty for now, will populate later
+
+
+supportedArchToArch :: SupportedArch -> Arch
+supportedArchToArch X64 = EM_x86_64
+
+supportedArchToClass :: SupportedArch -> Class
+supportedArchToClass X64 = C_64
+
+supportedArchToEncoding :: SupportedArch -> Encoding
+supportedArchToEncoding X64 = D_2LSB
