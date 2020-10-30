4467973bbcbc428ca4c71d05c22933f87bce2ee3
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-30 20:59:34 +0100

    nsc-codegen: compile `ret` to X64 opcodes

--- lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64.hs
@@ -5,21 +5,23 @@ import Language.NStar.Typechecker.Core
 import Language.NStar.CodeGen.Machine.Internal.Intermediate (InterOpcode(..))
 import Data.Located (unLoc, Located(..))
 import Language.NStar.CodeGen.Errors
+import Control.Monad.Except (throwError)
+import Internal.Error (internalError)
 
 compileX64 :: TypedProgram -> Compiler ()
 compileX64 = (fixupAddressesX64 =<<) . compileInterX64
 
 compileInterX64 :: TypedProgram -> Compiler [InterOpcode]
-compileInterX64 (TProgram stmts) = (<>) <$> compileStmtInterX64 . unLoc <$> stmts
+compileInterX64 (TProgram stmts) = mconcat <$> mapM (compileStmtInterX64 . unLoc) stmts
 
-compileInterStmtInterX64 :: TypedStatement -> Compiler [InterOpcode]
-compileInterStmtInterX64 (TLabel name) = pure [Label name]
-compileInterStmtInterX64 (TInstr i ts) = compileInterInstrX64 (unLoc i) (unLoc <$> ts)
+compileStmtInterX64 :: TypedStatement -> Compiler [InterOpcode]
+compileStmtInterX64 (TLabel name) = pure [Label (unLoc name)]
+compileStmtInterX64 (TInstr i ts) = compileInstrInterX64 (unLoc i) (unLoc <$> ts)
 
-compileInterInstrX64 :: Instruction -> [Type] -> Compiler [InterOpcode]
-compileInterInstrX64 Ret [] = pure [Byte 0xC3]
-compileInterInstrX64 Ret args = throwError (incorrectArgsTypesForInstr Ret args)
-compileInterInstrX64 i args = error $ "not yet implemented: compileInterInstrX64 " <> show i <> " " <> show args
+compileInstrInterX64 :: Instruction -> [Type] -> Compiler [InterOpcode]
+compileInstrInterX64 RET []   = pure [Byte 0xC3]
+compileInstrInterX64 RET args = internalError $ "Expected [] but got " <> show args <> " as arguments for " <> show RET
+compileInstrInterX64 i args   = error $ "not yet implemented: compileInterInstrX64 " <> show i <> " " <> show args
 
-fixupAddressesX64 :: [InterOpcode] -> Compile ()
+fixupAddressesX64 :: [InterOpcode] -> Compiler ()
 fixupAddressesX64 _ = pure ()
