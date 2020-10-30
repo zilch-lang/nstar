c37e9c3addd582008a9372d0a806f384bac89f30
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-30 17:04:36 +0100

    nsc-codegen: decompose X64 compilation into a two step phase

--- lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64.hs
@@ -2,6 +2,13 @@ module Language.NStar.CodeGen.Machine.X64 (compileX64) where
 
 import Language.NStar.CodeGen.Compiler
 import Language.NStar.Typechecker.Core
+import Language.NStar.CodeGen.Machine.Internal.Intermediate (InterOpcodes(..))
 
 compileX64 :: TypedProgram -> Compiler ()
-compileX64 _ = pure ()
+compileX64 p = compileInterX64 p >>= fixupAddressesX64
+
+compileInterX64 :: TypedProgram -> Compiler [InterOpcodes]
+compileInterX64 _ = pure []
+
+fixupAddressesX64 :: [InterOpcodes] -> Compile ()
+fixupAddressesX64 _ = pure ()
