4dd67dc8acd713d322b20b2c18a23533634d923f
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-30 20:59:01 +0100

    nsc-codegen: transform warnings into reports

--- lib/nsc-codegen/src/Language/NStar/CodeGen/Errors.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Errors.hs
@@ -1,3 +1,9 @@
 module Language.NStar.CodeGen.Errors where
 
+import Language.NStar.Typechecker.Core (Instruction, Type)
+import Text.Diagnose (Report, reportError, hint, Marker(..), prettyText)
+
 data CodegenError
+
+fromCodegenWarning :: CodegenError -> Report String
+fromCodegenWarning _ = undefined
--- lib/nsc-codegen/src/Language/NStar/CodeGen/Machine.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine.hs
@@ -5,13 +5,14 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS (pack)
 import Control.Monad.Writer (runWriter, execWriterT)
 import Language.NStar.CodeGen.Errors
-import Data.Bifunctor (second)
+import Data.Bifunctor (bimap)
 import Language.NStar.CodeGen.Machine.X64
+import Text.Diagnose (Report)
 
 data SupportedArch
   = X64
 
 
-compile :: SupportedArch -> TypedProgram -> ([CodegenError], ByteString)
-compile arch prog = second BS.pack $ runWriter $ execWriterT case arch of
+compile :: SupportedArch -> TypedProgram -> ([Report String], ByteString)
+compile arch prog = bimap (fmap fromCodegenWarning) BS.pack . runWriter $ execWriterT case arch of
   X64 -> compileX64 prog -- error "TODO: opcode compilation not yet implemented for x64"
