686919b7de7536cc0ea2e3a66afa8b6c5b122f1b
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-30 22:41:54 +0100

    nsc-codegen: return [Word8] instead of packing to a ByteString

--- lib/nsc-codegen/src/Language/NStar/CodeGen/Machine.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine.hs
@@ -1,14 +1,13 @@
 module Language.NStar.CodeGen.Machine where
 
 import Language.NStar.Typechecker.Core (TypedProgram)
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS (pack)
 import Control.Monad.Writer (execWriter)
 import Language.NStar.CodeGen.Machine.X64
 import Text.Diagnose (Report)
 import Language.NStar.CodeGen.Arch (SupportedArch(..))
+import Data.Word (Word8)
 
 
-compile :: SupportedArch -> TypedProgram -> ByteString
-compile arch prog = BS.pack $ execWriter case arch of
+compile :: SupportedArch -> TypedProgram -> [Word8]
+compile arch prog = execWriter case arch of
   X64 -> compileX64 prog -- error "TODO: opcode compilation not yet implemented for x64"
