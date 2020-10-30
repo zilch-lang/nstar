ed785d2b7b6d3f700a69773b24cadfdd2d35912c
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-30 17:35:59 +0100

    nsc-codegen: compile `ret` to X64 opcodes

--- lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64.hs
@@ -2,13 +2,24 @@ module Language.NStar.CodeGen.Machine.X64 (compileX64) where
 
 import Language.NStar.CodeGen.Compiler
 import Language.NStar.Typechecker.Core
-import Language.NStar.CodeGen.Machine.Internal.Intermediate (InterOpcodes(..))
+import Language.NStar.CodeGen.Machine.Internal.Intermediate (InterOpcode(..))
+import Data.Located (unLoc, Located(..))
+import Language.NStar.CodeGen.Errors
 
 compileX64 :: TypedProgram -> Compiler ()
-compileX64 p = compileInterX64 p >>= fixupAddressesX64
+compileX64 = (fixupAddressesX64 =<<) . compileInterX64
 
-compileInterX64 :: TypedProgram -> Compiler [InterOpcodes]
-compileInterX64 _ = pure []
+compileInterX64 :: TypedProgram -> Compiler [InterOpcode]
+compileInterX64 (TProgram stmts) = (<>) <$> compileStmtInterX64 . unLoc <$> stmts
 
-fixupAddressesX64 :: [InterOpcodes] -> Compile ()
+compileInterStmtInterX64 :: TypedStatement -> Compiler [InterOpcode]
+compileInterStmtInterX64 (TLabel name) = pure [Label name]
+compileInterStmtInterX64 (TInstr i ts) = compileInterInstrX64 (unLoc i) (unLoc <$> ts)
+
+compileInterInstrX64 :: Instruction -> [Type] -> Compiler [InterOpcode]
+compileInterInstrX64 Ret [] = pure [Byte 0xC3]
+compileInterInstrX64 Ret args = throwError (incorrectArgsTypesForInstr Ret args)
+compileInterInstrX64 i args = error $ "not yet implemented: compileInterInstrX64 " <> show i <> " " <> show args
+
+fixupAddressesX64 :: [InterOpcode] -> Compile ()
 fixupAddressesX64 _ = pure ()
