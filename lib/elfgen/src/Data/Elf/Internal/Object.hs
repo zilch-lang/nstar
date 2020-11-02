b0e4046f4676bb98daf5970dfe9c1cdc1cfdd825
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-02 21:18:26 +0100

    elfgen: serialize internal ELF object

--- lib/elfgen/src/Data/Elf/Internal/Object.hs
+++ lib/elfgen/src/Data/Elf/Internal/Object.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Data.Elf.Internal.Object where
 
@@ -8,6 +10,7 @@ import Data.Elf.Internal.SectionHeader
 import Data.Word (Word8)
 import GHC.TypeNats (Nat)
 import Data.Elf.Internal.BusSize (Size(..))
+import Data.Elf.Internal.Serialize (Serializable(..))
 
 data Object (n :: Size)
   = Obj
@@ -15,3 +18,14 @@ data Object (n :: Size)
       [Elf_Phdr n]   -- ^ Programs headers
       [Elf_Shdr n]   -- ^ Section headers
       [Word8]         -- ^ Raw data
+
+instance ( n ~ S64
+         , Serializable n e (Elf_Ehdr n)
+         , Serializable n e (Elf_Phdr n)
+         , Serializable n e (Elf_Shdr n)
+         ) => Serializable n e (Object n) where
+  put (Obj fileHeader programHeaders sectionHeaders binaryData) = do
+    put @n @e fileHeader
+    put @n @e programHeaders
+    put @n @e sectionHeaders
+    put @n @e binaryData
