e9f0c66cfd7431f11593fd24bfc3f602874e4f27
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-02 20:20:35 +0100

    elfgen: use newly created 'Size' datakind for 'Serializable'

--- lib/elfgen/src/Data/Elf/Internal/Serialize.hs
+++ lib/elfgen/src/Data/Elf/Internal/Serialize.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Data.Elf.Internal.Serialize
@@ -16,33 +17,53 @@ import Data.Kind (Type)
 import qualified Data.Binary.Put as B
 import Data.Word (Word8, Word16, Word32, Word64)
 import Data.Int (Int8, Int16, Int32, Int64)
+import Data.Elf.Internal.BusSize (Size(..))
+
+
+data Endianness
+  = LE     -- ^ little endian
+  | BE     -- ^ big endian
+
+
 
 -- | A class of serializable values depending on the endianness (le) and the data size (n).
 --
 --   n> @n@ should be equal to @32@ or @64@.
-class Serializable (n :: Nat) (le :: Bool) (a :: Type) where
+class Serializable (n :: Size) (le :: Endianness) (a :: Type) where
   put :: a -> B.Put
 
-instance Serializable 64 b Word8 where
+instance Serializable S32 e Word8 where
   put = B.putWord8
 
-instance Serializable 64 b Int8 where
-  put = put @64 @b @Word8 . fromIntegral
+instance Serializable S64 e Word8 where
+  put = B.putWord8
 
-instance Serializable 64 True Word16 where
+instance Serializable n e Word8 => Serializable n e Int8 where
+  put = put @n @e @Word8 . fromIntegral
+
+instance Serializable S64 LE Word16 where
   put = B.putWord16le
 
-instance Serializable 64 True Int16 where
-  put = put @64 @True @Word16 . fromIntegral
+instance Serializable S64 BE Word16 where
+  put = B.putWord16be
 
-instance Serializable 64 True Word32 where
+instance Serializable n e Word16 => Serializable n e Int16 where
+  put = put @n @e @Word16 . fromIntegral
+
+instance Serializable S64 LE Word32 where
   put = B.putWord32le
 
-instance Serializable 64 True Int32 where
-  put = put @64 @True @Word32 . fromIntegral
+instance Serializable S64 BE Word32 where
+  put = B.putWord32be
 
-instance Serializable 64 True Word64 where
+instance Serializable n e Word32 => Serializable n e Int32 where
+  put = put @n @e @Word32 . fromIntegral
+
+instance Serializable S64 LE Word64 where
   put = B.putWord64le
 
-instance Serializable 64 True Int64 where
-  put = put @64 @True @Word64 . fromIntegral
+instance Serializable S64 BE Word64 where
+  put = B.putWord64be
+
+instance Serializable n e Word64 => Serializable n e Int64 where
+  put = put @n @e @Word64 . fromIntegral
