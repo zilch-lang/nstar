a9e76188115eb5d7454e6928148d2522a8dbe3a6
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-03 09:02:57 +0100

    elfgen: serialize data no matter the bus size

--- lib/elfgen/src/Data/Elf/Internal/Serialize.hs
+++ lib/elfgen/src/Data/Elf/Internal/Serialize.hs
@@ -28,50 +28,50 @@ import Data.Elf.Internal.Endianness (Endianness(..))
 class Serializable (n :: Size) (le :: Endianness) (a :: Type) where
   put :: a -> B.Put
 
-instance Serializable S64 e Word8 where
+instance Serializable n e Word8 where
   put = B.putWord8
 
-instance Serializable S64 e Int8 where
+instance Serializable n e Int8 where
   put = B.putInt8
 
-instance Serializable S64 LE Word16 where
+instance Serializable n LE Word16 where
   put = B.putWord16le
 
-instance Serializable S64 BE Word16 where
+instance Serializable n BE Word16 where
   put = B.putWord16be
 
-instance Serializable S64 LE Int16 where
+instance Serializable n LE Int16 where
   put = B.putInt16le
 
-instance Serializable S64 BE Int16 where
+instance Serializable n BE Int16 where
   put = B.putInt16be
 
-instance Serializable S64 LE Word32 where
+instance Serializable n LE Word32 where
   put = B.putWord32le
 
-instance Serializable S64 BE Word32 where
+instance Serializable n BE Word32 where
   put = B.putWord32be
 
-instance Serializable S64 LE Int32 where
+instance Serializable n LE Int32 where
   put = B.putInt32le
 
-instance Serializable S64 BE Int32 where
+instance Serializable n BE Int32 where
   put = B.putInt32be
 
-instance Serializable S64 LE Word64 where
+instance Serializable n LE Word64 where
   put = B.putWord64le
 
-instance Serializable S64 BE Word64 where
+instance Serializable n BE Word64 where
   put = B.putWord64be
 
-instance Serializable S64 LE Int64 where
+instance Serializable n LE Int64 where
   put = B.putInt64le
 
-instance Serializable S64 BE Int64 where
+instance Serializable n BE Int64 where
   put = B.putInt64be
 
-instance Serializable S64 b a => Serializable S64 b [a] where
-  put = mapM_ (put @S64 @b)
+instance Serializable n e a => Serializable n e [a] where
+  put = mapM_ (put @n @e)
     -- A little tradeoff happening here: most serializers put the size of the list in front of the list.
     -- While this works, this is not what I want because the ELF format does not do that.
     -- That's why we simply output everything concatenated.
