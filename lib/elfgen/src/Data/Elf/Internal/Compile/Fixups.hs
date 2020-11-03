234cd7bd458a825f2a4edf4fd0bd519e70c220bf
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-03 22:13:37 +0100

    elfgen: fix section names indices in .shstrtab section

--- lib/elfgen/src/Data/Elf/Internal/Compile/Fixups.hs
+++ lib/elfgen/src/Data/Elf/Internal/Compile/Fixups.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Data.Elf.Internal.Compile.Fixups
 ( FixupEnvironment(..), Fixup
@@ -21,8 +22,12 @@ import qualified Data.Text as Text
 import Control.Monad.State (State, get, put, execState)
 import Data.Functor ((<&>))
 import Data.Elf.Internal.BusSize (Size(..))
-import Data.List (elemIndex)
-import Data.Elf.Types (ValueSet)
+import Data.List (elemIndex, intercalate)
+import Data.Elf.Types (ValueSet, Elf_Off)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Word (Word8)
+import Unsafe.Coerce (unsafeCoerce)
 
 -- | Associative list between abstract and concrete section header structures.
 type SectionAList n = Map (SectionHeader n) (Elf_Shdr n)
@@ -38,6 +43,7 @@ data FixupEnvironment (n :: Size)
       (SectionAList n)     -- ^ An association list associating abstract and concrete section headers
       (SectionByName n)    -- ^ All segments mapped by their respective names
       (SegmentAList n)     -- ^ An association from abstract to concrete segment headers
+      ByteString           -- ^ All generated binary data unrelated to headers (for example the content of the @.shstrtab@ section)
 
 type Fixup (n :: Size) a = State (FixupEnvironment n) a
 
@@ -53,24 +59,25 @@ allFixes = do
   fixupHeadersOffsets
   fixupPHDREntry
   fixupSectionNames
+  fixupSectionOffsets
 
 -- | A fix for headers count in the ELF file header (fields 'e_phnum' and 'e_shnum').
 fixupHeaderCount :: ValueSet n => Fixup n ()
 fixupHeaderCount = do
-  FixupEnv fileHeader sects sectsNames segs <- get
+  FixupEnv fileHeader sects sectsNames segs gen <- get
   let newHeader = fileHeader
         { e_phnum = fromIntegral (Map.size segs)
         , e_shnum = fromIntegral (Map.size sects) }
-  put (FixupEnv newHeader sects sectsNames segs)
+  put (FixupEnv newHeader sects sectsNames segs gen)
 
 -- | Fixes the @.shstrtab@ section index in the file header.
 fixupShstrtabIndex :: ValueSet n => Fixup n ()
 fixupShstrtabIndex = do
-  FixupEnv fileHeader sects sectsNames segs <- get
+  FixupEnv fileHeader sects sectsNames segs gen <- get
   let Just e_shstrtabndx  = elemIndex ".shstrtab" (getName <$> Map.keys sects)
       newHeader           = fileHeader
         { e_shstrndx = fromIntegral e_shstrtabndx }
-  put (FixupEnv newHeader sects sectsNames segs)
+  put (FixupEnv newHeader sects sectsNames segs gen)
  where
    getName SNull             = ""
    getName (SProgBits n _ _) = n
@@ -80,7 +87,7 @@ fixupShstrtabIndex = do
 -- | Fixes (sections/segments) headers offsets in the file header.
 fixupHeadersOffsets :: ValueSet n => Fixup n ()
 fixupHeadersOffsets = do
-  FixupEnv fileHeader sects sectsNames segs <- get
+  FixupEnv fileHeader sects sectsNames segs gen <- get
   let phnum     = fromIntegral (e_phnum fileHeader)
       phentsize = fromIntegral (e_phentsize fileHeader)
       phoff     = fromIntegral (e_ehsize fileHeader)    -- we want to have program headers right after the file header
@@ -89,7 +96,7 @@ fixupHeadersOffsets = do
         { e_shoff = shoff
         , e_phoff = phoff }
 
-  put (FixupEnv newHeader sects sectsNames segs)
+  put (FixupEnv newHeader sects sectsNames segs gen)
 
 -- | Fixes the @LOAD@ segment corresponding to the @PHDR@ segment
 --
@@ -97,7 +104,7 @@ fixupHeadersOffsets = do
 --   memory segment.
 fixupPHDREntry :: ValueSet n => Fixup n ()
 fixupPHDREntry = do
-  FixupEnv fileHeader sects sectsNames segs <- get
+  FixupEnv fileHeader sects sectsNames segs gen <- get
 
   let phoff    = fromIntegral (e_phoff fileHeader)
       phdrSize = fromIntegral (e_phnum fileHeader) * fromIntegral (e_phentsize fileHeader)
@@ -114,13 +121,13 @@ fixupPHDREntry = do
                 Map.update (const phdrLoad) (PLoad (section "PHDR") pf_r) $
                 segs
 
-  put (FixupEnv fileHeader sects sectsNames newSegs)
+  put (FixupEnv fileHeader sects sectsNames newSegs gen)
 
 -- | Fixes every section name index (field 'sh_name') depending on how data is laid out
 --   in the @.shstrtab@ section.
 fixupSectionNames :: ValueSet n => Fixup n ()
 fixupSectionNames = do
-  FixupEnv fileHeader sects sectsNames segs <- get
+  FixupEnv fileHeader sects sectsNames segs gen <- get
 
   let names = Map.keys sectsNames
   let newSects = Map.fromList $ names <&> \ n ->
@@ -131,12 +138,61 @@ fixupSectionNames = do
         in (s, sh)
   let newSectsWithUnmodified = Map.union newSects sects
 
-  put (FixupEnv fileHeader newSectsWithUnmodified sectsNames segs)
+  put (FixupEnv fileHeader newSectsWithUnmodified sectsNames segs gen)
  where
    fetchStringIndex = goFetch 0
 
    goFetch :: Int -> Text -> [Text] -> Int
    goFetch n str [] = 0
    goFetch n str (x:xs)
-     | str == x  = n
+     | str == x  = n + 1
      | otherwise = goFetch (n + 1 + Text.length x) str xs
+
+-- | Inserts binary data from a section (if it contains any) directly into the environment
+--   and updates the fields 'sh_addr', 'sh_offset' and 'sh_size'.
+fixupSectionOffsets :: ValueSet n => Fixup n ()
+fixupSectionOffsets = do
+  FixupEnv fileHeader sects sectsNames segs gen <- get
+
+  let startSects = e_shoff fileHeader
+      sectsCount = fromIntegral (e_shnum fileHeader)
+      sectsSize  = fromIntegral (e_shentsize fileHeader)
+      endSects   = startSects + sectsCount * sectsSize
+  let startPhys = endSects
+
+  let (newGen, newSects) = generateBinFromSectionsStartingAt (Map.toList sects) startPhys
+  let newGenBin = gen <> newGen
+      newSectsModif = Map.union newSects sects
+
+  put (FixupEnv fileHeader newSectsModif sectsNames segs newGenBin)
+ where
+   generateBinFromSectionsStartingAt :: ValueSet n
+                                     => [(SectionHeader n, Elf_Shdr n)]        -- ^ mappings unifying sections
+                                     -> Elf_Off n                              -- ^ starting file offset
+                                     -> (ByteString, Map (SectionHeader n) (Elf_Shdr n))
+   generateBinFromSectionsStartingAt [] off               = (mempty, mempty)
+   generateBinFromSectionsStartingAt (s@(sh, shd):ss) off =
+     let (newOff, sectBin, newSects) = case s of
+           (SNull, _)           -> (off, mempty, Map.singleton sh shd)
+           (SProgBits _ d _, _) ->
+             let size       = fromIntegral (length d)
+                 packed     = BS.pack d
+                 addr       = 0x400000 + fromIntegral off
+                 updatedShd = shd { sh_offset = off, sh_addr = addr, sh_size = size }
+             in (off + fromIntegral size, packed, Map.singleton sh updatedShd)
+           (SNoBits _ s _, _)   ->
+             let addr       = 0x400000 + fromIntegral off
+                 updatedShd = shd { sh_offset = off, sh_addr = addr, sh_size = fromIntegral s }
+             in (off, mempty, Map.singleton sh updatedShd)
+           (SStrTab _ s, _)     ->
+             let content    = c2w <$> ('\0' : (intercalate "\0" s <> "\0"))
+                 size       = fromIntegral (length content)
+                 packed     = BS.pack content
+                 updatedShd = shd { sh_offset = off, sh_size = size }
+             in (off + fromIntegral size, packed, Map.singleton sh updatedShd)
+
+         (binGen, sects) = generateBinFromSectionsStartingAt ss newOff
+     in (sectBin <> binGen, Map.union newSects sects)
+
+   c2w :: Char -> Word8
+   c2w = unsafeCoerce
