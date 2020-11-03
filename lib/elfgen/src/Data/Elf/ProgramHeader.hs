5a35d6a4539271b0b93640eb3b0f45d250633156
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-03 16:33:44 +0100

    elfgen: make self-explanatory type alias 'SectionOrData'

--- lib/elfgen/src/Data/Elf/ProgramHeader.hs
+++ lib/elfgen/src/Data/Elf/ProgramHeader.hs
@@ -19,7 +19,7 @@ data ProgramHeader n
   | PNull
   -- | Loadable program segment
   | PLoad
-      (Either String [Word8])     -- ^ Either the name of a section or concrete binary data.
+      SectionOrData     -- ^ Either the name of a section or concrete binary data.
       (PFlags n)
   -- | Program interpreter
   | PInterp
@@ -28,11 +28,14 @@ data ProgramHeader n
 deriving instance ValueSet n => Eq (ProgramHeader n)
 deriving instance ValueSet n => Ord (ProgramHeader n)
 
+-- | Either a section name, or concrete binary data.
+type SectionOrData = Either String [Word8]
+
 -- | Indicates that we want to fetch data from a specific section. This essentially is an alias for 'Left', to be used with data constructors from 'ProgramHeader'
 --   in order to prevent duplicated binary data in the resulting ELF file (note that this duplication causes all sorts of problems like non-allocation of progbits).
-section :: String -> Either String [Word8]
+section :: String -> SectionOrData
 section = Left
 
 -- | Indicates that we want to have direct binary data which is not present in any other section. This is an alias for 'Right'.
-binaryData :: [Word8] -> Either String [Word8]
+binaryData :: [Word8] -> SectionOrData
 binaryData = Right
