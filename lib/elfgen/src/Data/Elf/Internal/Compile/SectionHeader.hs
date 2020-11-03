1ddbbf3153d4443d2a7d9383c411ef7352cd4325
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-03 16:17:38 +0100

    elfgen: make 'SectionHeader' and 'Elf_Shdr' size-coercible

--- lib/elfgen/src/Data/Elf/Internal/Compile/SectionHeader.hs
+++ lib/elfgen/src/Data/Elf/Internal/Compile/SectionHeader.hs
@@ -3,46 +3,47 @@ module Data.Elf.Internal.Compile.SectionHeader where
 import Data.Elf.SectionHeader
 import Data.Elf.Internal.SectionHeader
 import Data.Elf.Internal.BusSize (Size(..))
+import Data.Elf.Internal.Compile.ForArch
 
--- | Compiles an abstract 'SectionHeader' into a concrete 'Elf64_Shdr'.
---
---   __NOTE:__ Some fields are not filled, because of the lack of information at that current point.
---             These are listed here:
---
---             - 'sh_name'
---             - 'sh_addr'
---             - 'sh_offset'
---             - 'sh_size'
---             - 'sh_link'
---             - 'sh_info'
---             - 'sh_addralign'
---             - 'sh_entsize' (may not be filled if the section does not hold a table)
-compileSectionHeader64bits :: SectionHeader -> Elf_Shdr S64
-compileSectionHeader64bits sect =
-  Elf64_Shdr
-    0x0
-    compileType64bits
-    (fromIntegral compileFlags64bits)
-    0x0
-    0x0
-    0x0
-    0x0
-    0x0
-    compileAlignment64bits
-    0x0
-  where
-    compileType64bits = case sect of
-      SNull           -> sht_null
-      SProgBits _ _ _ -> sht_progbits
-      SNoBits _ _ _   -> sht_nobits
-      SStrTab _ _     -> sht_strtab
-    compileFlags64bits = case sect of
-      SNull           -> 0x0
-      SProgBits _ _ f -> f
-      SNoBits _ _ f   -> f
-      SStrTab _ _     -> 0x0
-    compileAlignment64bits = case sect of
-      SNull           -> 0x0
-      SProgBits _ _ _ -> 0x1
-      SNoBits _ _ _   -> 0x20
-      SStrTab _ _     -> 0x1
+instance CompileFor S64 SectionHeader Elf_Shdr where
+  -- | Compiles an abstract 'SectionHeader' into a concrete 'Elf_Shdr'.
+  --
+  --   __NOTE:__ Some fields are not filled, because of the lack of information at that current point.
+  --             These are listed here:
+  --
+  --             - 'sh_name'
+  --             - 'sh_addr'
+  --             - 'sh_offset'
+  --             - 'sh_size'
+  --             - 'sh_link'
+  --             - 'sh_info'
+  --             - 'sh_addralign'
+  --             - 'sh_entsize' (may not be filled if the section does not hold a table)
+  compileFor sect =
+    Elf_Shdr @S64
+      0x0
+      compileType64bits
+      (fromIntegral compileFlags64bits)
+      0x0
+      0x0
+      0x0
+      0x0
+      0x0
+      compileAlignment64bits
+      0x0
+    where
+      compileType64bits = case sect of
+        SNull           -> sht_null @S64
+        SProgBits _ _ _ -> sht_progbits @S64
+        SNoBits _ _ _   -> sht_nobits @S64
+        SStrTab _ _     -> sht_strtab @S64
+      compileFlags64bits = case sect of
+        SNull           -> 0x0
+        SProgBits _ _ f -> f
+        SNoBits _ _ f   -> f
+        SStrTab _ _     -> 0x0
+      compileAlignment64bits = case sect of
+        SNull           -> 0x0
+        SProgBits _ _ _ -> 0x1
+        SNoBits _ _ _   -> 0x20
+        SStrTab _ _     -> 0x1
