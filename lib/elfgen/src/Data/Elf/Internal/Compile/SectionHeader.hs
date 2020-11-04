397e75a04f001677f4ca6534bcb1d98275dc27cc
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-05 00:28:11 +0100

    elfgen: fill (entry) sizes on symbol table section

--- lib/elfgen/src/Data/Elf/Internal/Compile/SectionHeader.hs
+++ lib/elfgen/src/Data/Elf/Internal/Compile/SectionHeader.hs
@@ -4,6 +4,8 @@ import Data.Elf.SectionHeader
 import Data.Elf.Internal.SectionHeader
 import Data.Elf.Internal.BusSize (Size(..))
 import Data.Elf.Internal.Compile.ForArch
+import Data.Elf.Internal.Symbol
+import Foreign.Storable (sizeOf)
 
 instance CompileFor S64 SectionHeader Elf_Shdr where
   -- | Compiles an abstract 'SectionHeader' into a concrete 'Elf_Shdr'.
@@ -14,11 +16,10 @@ instance CompileFor S64 SectionHeader Elf_Shdr where
   --             - 'sh_name'
   --             - 'sh_addr'
   --             - 'sh_offset'
-  --             - 'sh_size'
+  --             - 'sh_size' (may not be filled if size cannot be determined here)
   --             - 'sh_link'
   --             - 'sh_info'
   --             - 'sh_addralign'
-  --             - 'sh_entsize' (may not be filled if the section does not hold a table)
   compileFor sect =
     Elf_Shdr @S64
       0x0
@@ -26,24 +27,39 @@ instance CompileFor S64 SectionHeader Elf_Shdr where
       (fromIntegral compileFlags64bits)
       0x0
       0x0
-      0x0
+      compileSize64bits
       0x0
       0x0
       compileAlignment64bits
-      0x0
+      compileEntsize64bits
     where
       compileType64bits = case sect of
         SNull           -> sht_null @S64
         SProgBits _ _ _ -> sht_progbits @S64
         SNoBits _ _ _   -> sht_nobits @S64
         SStrTab _ _     -> sht_strtab @S64
+        SSymTab _ _     -> sht_symtab @S64
       compileFlags64bits = case sect of
         SNull           -> 0x0
         SProgBits _ _ f -> f
         SNoBits _ _ f   -> f
         SStrTab _ _     -> 0x0
+        SSymTab _ _     -> 0x0
       compileAlignment64bits = case sect of
         SNull           -> 0x0
         SProgBits _ _ _ -> 0x1
         SNoBits _ _ _   -> 0x20
         SStrTab _ _     -> 0x1
+        SSymTab _ _     -> 0x8
+      compileEntsize64bits = case sect of
+        SNull           -> 0x0
+        SProgBits _ _ _ -> 0x0
+        SNoBits _ _ _   -> 0x0
+        SStrTab _ _     -> 0x0
+        SSymTab _ _     -> fromIntegral $ sizeOf @(Elf_Sym S64) undefined
+      compileSize64bits = case sect of
+        SNull           -> 0x0
+        SProgBits _ _ _ -> 0x0
+        SNoBits _ _ _   -> 0x0
+        SStrTab _ _     -> 0x0
+        SSymTab _ s     -> fromIntegral (sizeOf @(Elf_Sym S64) undefined) * fromIntegral (length s)
