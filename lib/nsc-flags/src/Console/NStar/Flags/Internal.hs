cd6bb5fe5e50cdad1e2848e7cd8de41e682021b9
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-08 14:14:52 +0100

    nsc-flags: store configuration in separate record

--- lib/nsc-flags/src/Console/NStar/Flags/Internal.hs
+++ lib/nsc-flags/src/Console/NStar/Flags/Internal.hs
@@ -1,31 +1,23 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE NamedFieldPuns #-}
-
 module Console.NStar.Flags.Internal where
-
-import Data.Data (Data)
-import Data.Typeable (Typeable)
-import Data.Map (Map)
-import qualified Data.Map as Map
-
-data CompilerFlags
-  = CFlags
-  { files :: [FilePath]
-  , flags :: Map String String
-  }
- deriving (Show)
-
-lookupFlag :: String -> CompilerFlags -> Maybe String
-lookupFlag name CFlags{flags} = Map.lookup name flags
-
 
 data Flags
   = Flags
-  { files  :: [FilePath]      -- ^ The path to all the files to compile.
-  , flags  :: [String]        -- ^ A set of compiler flags.
+  { files         :: [FilePath]
+  , configuration :: ConfigurationFlags
   }
- deriving (Data, Typeable, Show)
+ deriving (Show)
+
+data ConfigurationFlags
+  = ConfigFlags
+  { diagnostic_color :: Bool
+  }
+  deriving (Show)
+
+instance Semigroup ConfigurationFlags where
+  ConfigFlags dc1 <> ConfigFlags dc2 = ConfigFlags (dc1 || dc2)
+
+instance Monoid ConfigurationFlags where
+  mempty = ConfigFlags False
 
 -------------------------------------------------------------------------------
 
