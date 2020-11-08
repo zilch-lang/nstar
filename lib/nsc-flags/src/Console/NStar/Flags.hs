e914a51d92aad8d3b17ca519b1b99597d7d87833
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-08 14:15:38 +0100

    nsc-flags: parse configuration bindings in -f option

--- lib/nsc-flags/src/Console/NStar/Flags.hs
+++ lib/nsc-flags/src/Console/NStar/Flags.hs
@@ -1,24 +1,90 @@
-{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE ApplicativeDo #-}
 
 module Console.NStar.Flags
 ( extractFlags
 ,  -- * Re-exports
-  CompilerFlags(..)
-, lookupFlag
+  Flags(..), ConfigurationFlags(..)
 , LexerFlags(..), ParserFlags(..), TypecheckerFlags(..)
 ) where
 
 import Console.NStar.Flags.Internal
-import Console.NStar.Flags.Process
-import System.Console.CmdArgs
+import Options.Applicative
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Text.Megaparsec as Mega
+import qualified Text.Megaparsec.Char as MegaC
+import Data.Data (Data)
+import Data.Typeable (Typeable)
+import Data.Char (toLower)
+import qualified Data.Set as Set
+import Data.Foldable (fold)
+import Data.Bifunctor (first)
+import Control.Monad (join)
+import Data.List (intercalate)
 
-extractFlags :: IO CompilerFlags
-extractFlags = postProcessFlags =<< cmdArgsRun (cmdArgsMode cli)
+extractFlags :: IO Flags
+extractFlags = execParser opts
+  where
+    opts = info (cli <**> helper) (fullDesc)
 
-cli :: Flags
-cli = Flags
-  { files = def &= args &= typ "FILES..."
-  , flags = def &= typ "NAME=VALUE" &= explicit &= name "f" &= name "flag" &= help "Sets a compiler flag to a given value"
-  } &= versionArg [ignore]
-    &= helpArg [explicit, name "help", name "h"]
-    &= program "nsc"
+cli :: Parser Flags
+cli =
+  Flags
+    <$> many (argument str (metavar "FILES..."))
+    <*> (mconcat <$> many config)
+
+config :: Parser ConfigurationFlags
+config = do
+  fl <- option (eitherReader parseConfigFlag) (long "flag" <> short 'f' <> metavar "OPTION[=VALUE]")
+  pure $ mempty
+    { diagnostic_color = maybe False fromYesNo (join $ Map.lookup "color-diagnostics" fl) }
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------------
+
+fromYesNo :: String -> Bool
+fromYesNo "yes" = True
+fromYesNo "no"  = False
+
+parseConfigFlag :: String -> Either String (Map String (Maybe String))
+parseConfigFlag = first toStringError . Mega.runParser configFlags "cli-config"
+  where
+    toStringError Mega.ParseErrorBundle{..} =
+      fold (Mega.parseErrorTextPretty <$> bundleErrors)
+
+data ErrorContext
+  = ErrCtx
+  { inFields :: String
+  , err      :: Mega.ParseError String ErrorContext
+  }
+  deriving (Eq, Show, Typeable, Data)
+instance Ord ErrorContext where
+  ErrCtx _ e1 <= ErrCtx _ e2 = off e1 <= off e2
+    where
+      off (Mega.TrivialError o _ _) = o
+      off (Mega.FancyError o _)     = o
+
+instance Mega.ShowErrorComponent ErrorContext where
+  showErrorComponent ErrCtx{..} =
+    intercalate " " (drop 1 (lines (Mega.parseErrorTextPretty err))) <> " in configuration definition for key '" <> inFields <> "'\n"
+
+configFlags :: Mega.Parsec ErrorContext String (Map String (Maybe String))
+configFlags = uncurry Map.singleton <$> Mega.choice
+  [ field "color-diagnostics" yesno, unknownField ]
+
+field :: String -> Mega.Parsec ErrorContext String String -> Mega.Parsec ErrorContext String (String, Maybe String)
+field name val = Mega.region (\ p@(Mega.TrivialError o _ _) -> Mega.FancyError o . Set.singleton . Mega.ErrorCustom $ ErrCtx name p) $
+  (,) <$> MegaC.string name
+      <*> Mega.optional (MegaC.char '=' *> val)
+
+unknownField :: Mega.Parsec ErrorContext String (String, Maybe String)
+unknownField = fail . (\ k -> "Unknown configuration key '" <> k <> "'") =<< Mega.takeWhileP Nothing (/= '=')
+
+yesno :: Mega.Parsec ErrorContext String String
+yesno = fmap toLower <$> Mega.choice [ MegaC.string' "yes", MegaC.string' "no" ]
