a227b92d212b46decad48f412f4cae6aac615ca7
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-10 23:41:44 +0100

    nsc-branchchecker: start with generating jump edges between labels

---
+++ lib/nsc-branchchecker/src/Language/NStar/Branchchecker/Check.hs
@@ -0,0 +1,56 @@
+module Language.NStar.Branchchecker.Check (branchcheck) where
+
+import Language.NStar.Branchchecker.FlowGraph
+import Algebra.Graph.Labelled.AdjacencyMap ((-<), (>-))
+import qualified Algebra.Graph.Labelled.AdjacencyMap as Graph
+import Text.Diagnose (Diagnostic, diagnostic, (<++>))
+import Control.Monad.Except
+import Control.Monad.State
+import Language.NStar.Branchchecker.Errors
+import Language.NStar.Typechecker.Core
+import Data.Bifunctor (first, second)
+import Control.Monad (forM_)
+import Data.Located (Located(..), unLoc)
+import Data.Text (Text)
+import Data.List (foldl')
+import Data.Functor ((<&>))
+import qualified Data.Set as Set
+
+type Checker a = StateT (Maybe (Located Text), JumpGraph) (Except BranchcheckerError) a
+
+branchcheck :: TypedProgram -> Either (Diagnostic s String m) ()
+branchcheck p = first toDiagnostic $ runExcept (evalStateT (branchcheckProgram p) (Nothing, "_start"-<Call>-"main"))
+  where toDiagnostic = (diagnostic <++>) . fromBranchcheckerError                              --  ^^^^^^^^^^^ "a-<i>-b" really means "a -i-> b"
+
+branchcheckProgram :: TypedProgram -> Checker ()
+branchcheckProgram (TProgram stts) = do
+  -- set current scope to Nothing
+  forM_ stts registerEdges
+
+registerEdges :: Located TypedStatement -> Checker ()
+registerEdges (TLabel name :@ _) =
+  modify (first (const (Just name)))
+registerEdges (TInstr i _ :@ p) = case unLoc i of
+  RET -> do
+    (lbl, graph) <- get
+    let Just (label :@ _) = lbl
+
+    -- fetch upwards the last call that got to the current label
+    -- and insert an edge for each of the parents of those calls.
+    let parents  = fetchAllCallParents label graph
+        newGraph = foldl' (\ g p -> Graph.overlay g (label-<Ret>-p)) graph parents
+    modify (second (const newGraph))
+
+    pure ()
+
+  -- other instruction do not act on the control flow.
+  _ -> pure ()
+
+fetchAllCallParents :: Text -> JumpGraph -> [Text]
+fetchAllCallParents root graph =
+  let predec  = Graph.preSet root graph
+      labels  = Set.toList predec <&> \ p -> (p, Graph.edgeLabel p root graph)
+      parents = labels >>= \ (p, l) -> case l of
+        Call -> [p]
+        _    -> fetchAllCallParents p (Graph.removeEdge p root graph)
+  in parents
---
+++ lib/nsc-branchchecker/src/Language/NStar/Branchchecker/Errors.hs
@@ -0,0 +1,22 @@
+module Language.NStar.Branchchecker.Errors where
+
+import Data.Located
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Text.Diagnose (Report, Marker(..), hint, reportError, prettyText)
+
+data BranchcheckerError
+  = ControlFlowLeak (Located Text) (Located Text)
+
+fromBranchcheckerError :: BranchcheckerError -> Report String
+fromBranchcheckerError (ControlFlowLeak (from :@ p1) (to :@ p2)) = controlFlowLeak (from, p1) (to, p2)
+
+controlFlowLeak :: (Text, Position) -> (Text, Position) -> Report String
+controlFlowLeak (from, p1) (to, p2@(Position (l1, _) _ file)) =
+  reportError ("Control flow leak from scope of label '" <> Text.unpack from <> "' into scope of label '" <> Text.unpack to <> "'.")
+    [ (p1, Where "Leaked from here")
+    , (p2, Where "Leaked into here")
+    , (p3, Maybe $ "Consider adding `jmp " <> Text.unpack to <> "` here to prevent the leak")]
+    [ hint "There is no way of typechecking a program where control flow leaks through labels." ]
+  where
+    p3 = Position (l1 - 1, 1) (l1 - 1, 1) file
