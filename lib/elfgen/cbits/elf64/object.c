de708b4fb4cfe4a386d742af4fc26333f25ac3a4
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-01-17 14:00:55 +0100

    elfgen: free inserted relocation symbols

--- lib/elfgen/cbits/elf64/object.c
+++ lib/elfgen/cbits/elf64/object.c
@@ -24,5 +24,11 @@ void free_elf64_object(Elf64_Object *obj)
     }
     free(obj->symbols);
 
+    for (unsigned int i = 0; i < obj->relocations_len; ++i)
+    {
+        free(obj->relocations[i]);
+    }
+    free(obj->relocations);
+
     free(obj->binary_data);
 }
