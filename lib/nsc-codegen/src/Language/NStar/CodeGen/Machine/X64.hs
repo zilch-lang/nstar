7d337f9690df5001f8d69afe880cfe081ec3e828
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-01-24 00:30:09 +0100

    nsc-codegen: compile 'mov %rN(%rM), %rP' on x64

--- lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64.hs
@@ -94,6 +94,9 @@ compileInstrInterX64 (MOV (Indexed (Imm (I disp :@ _) :@ _) (Name l :@ _) :@ _) 
 -- REX.W + 8B /r	MOV r64,r/m64	        RM	Valid	        N.E.	                Move r/m64 to r64.
 compileInstrInterX64 (MOV (Indexed (Imm (I disp :@ _) :@ _) (Reg src :@ _) :@ _) (Reg dst :@ _)) [_, Register 64] =
   pure $ [rexW, Byte 0x8B, modRM 0x1 (registerNumber (unLoc dst)) (registerNumber (unLoc src))] <> (Byte <$> int8 disp)
+-- REX.W + 8B /r	MOV r64,r/m64	        RM	Valid	        N.E.	                Move r/m64 to r64.
+compileInstrInterX64 (MOV (Indexed (Reg r1 :@ _) (Reg r2 :@ _) :@ _) (Reg dst :@ _)) [_, _] =
+  pure $ [rexW, Byte 0x8B, modRM 0x0 (registerNumber (unLoc dst)) 0x4, sib 0x0 (registerNumber (unLoc r1)) (registerNumber (unLoc r2))]
 -- REX.W + B8+ rd io	MOV r64, imm64	        OI	Valid	        N.E.	                Move imm64 to r64.
 compileInstrInterX64 (MOV src@(Imm _ :@ _) (Reg dst :@ _)) [Unsigned 64, Register 64] =
   ([rexW, Byte (0xB8 + registerNumber (unLoc dst))] <>) <$> compileExprX64 64 (unLoc src)
