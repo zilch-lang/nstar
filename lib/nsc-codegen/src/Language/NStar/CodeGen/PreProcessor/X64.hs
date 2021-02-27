52d16c4567970aa82e47ff205742988938c6fe40
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-02-27 11:35:22 +0100

    nsc-codegen: remove preprocessing stages

--- lib/nsc-codegen/src/Language/NStar/CodeGen/PreProcessor/X64.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/PreProcessor/X64.hs
@@ -7,16 +7,4 @@ import Language.NStar.Typechecker.Core
 import Data.Located (Located((:@)))
 
 preprocessX64 :: TypedProgram -> TypedProgram
-preprocessX64 = patchMovRM2RM
-
--- | Patches the instruction @mov \@r\/m64 \@r\/m64@, because there is no corresponding opcode.
-patchMovRM2RM :: TypedProgram -> TypedProgram
-patchMovRM2RM (TProgram d rd ud (TCode c :@ p)) = TProgram d rd ud (TCode newC :@ p)
-  where
-    newC = c >>= transform
-
-    transform (TInstr (MV src@(IndexedE _ _ :@ _) dst@(IndexedE _ _ :@ _) :@ p1) chi sigma epsilon :@ p2) =
-      let r5 = RegE (R5 :@ p1) :@ p1
-      in [ TInstr (MV src r5 :@ p1) chi sigma epsilon :@ p2
-         , TInstr (MV r5 dst :@ p1) chi sigma epsilon :@ p2 ]
-    transform i = pure i
+preprocessX64 = id
