521d24cdefca25a95d7950fae376e4d740f3d965
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-03-07 09:20:23 +0100

    nsc-codegen: generate a positive offset for the 'sst' instruction

--- lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64/Sst.hs
+++ lib/nsc-codegen/src/Language/NStar/CodeGen/Machine/X64/Sst.hs
@@ -12,7 +12,7 @@ import Language.NStar.CodeGen.Machine.Internal.X64.RegisterEncoding (registerNum
 import Data.Located (unLoc)
 
 compileSst :: Expr -> Integer -> Compiler [InterOpcode]
-compileSst src@(ImmE _) n = mappend ([rexW, Byte 0xC7, modRM 0b1 0x0 0x4 {- RSP -}, sib 0x0 0x4 0x4] <> (Byte <$> int8 (-n))) <$> compileExprX64 32 src
-compileSst (RegE r) n = pure $ [rexW, Byte 0x89, modRM 0b1 0x4 (registerNumber (unLoc r)), sib 0x0 0x4 0x4] <> (Byte <$> int8 (-n))
-compileSst (NameE l _) n = pure $ [rexW, Byte 0xC7, modRM 0b1 0x0 0x4 {- RSP -}, sib 0x0 0x4 0x4] <> (Byte <$> int8 (-n)) <> [Symbol32 (unLoc l) 0]
+compileSst src@(ImmE _) n = mappend ([rexW, Byte 0xC7, modRM 0b1 0x0 0x4 {- RSP -}, sib 0x0 0x4 0x4] <> (Byte <$> int8 n)) <$> compileExprX64 32 src
+compileSst (RegE r) n = pure $ [rexW, Byte 0x89, modRM 0b1 0x4 (registerNumber (unLoc r)), sib 0x0 0x4 0x4] <> (Byte <$> int8 n)
+compileSst (NameE l _) n = pure $ [rexW, Byte 0xC7, modRM 0b1 0x0 0x4 {- RSP -}, sib 0x0 0x4 0x4] <> (Byte <$> int8 n) <> [Symbol32 (unLoc l) 0]
 compileSst s n = internalError $ "Unsupported instruction 'sst " <> show s <> ", " <> show n <> "'."
