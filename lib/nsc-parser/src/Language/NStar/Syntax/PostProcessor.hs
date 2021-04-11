614c16cfab97387decc213389d5ab3e4586a8c7b
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-04-11 18:07:11 +0200

    nsc-parser: transform top-level literal constants into array constants

--- lib/nsc-parser/src/Language/NStar/Syntax/PostProcessor.hs
+++ lib/nsc-parser/src/Language/NStar/Syntax/PostProcessor.hs
@@ -2,6 +2,7 @@ module Language.NStar.Syntax.PostProcessor where
 
 import Language.NStar.Syntax.Core (Program)
 import Language.NStar.Syntax.PostProcess.ConcatenateSections (concatSections)
+import Language.NStar.Syntax.PostProcess.TopLevelConstants (desugarTopLevelConstants)
 
 postProcessAST :: Program -> Program
-postProcessAST = {- expandMacros . -} concatSections {- . resolveTargets -}
+postProcessAST = {- expandMacros . -} desugarTopLevelConstants . concatSections {- . resolveTargets -}
